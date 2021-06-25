dbc820f67fb66dcea62200cf4c5f9effef8f6dcc
dis0ps <dis0ps.email@gmail.com>
2021-06-25 09:26:50 -0400

    dynamically create a route map for serving variable directory structures

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,21 +1,38 @@
 import * as fs from "fs";
+import * as path from "path";
 import * as http from "http";
 import * as config from "../config/AppConfig";
 
 export class Router {
-  constructor() {}
+  private routeMap: config.IRoute = {};
+
+  constructor() {
+    this.createRouteMap(config.PUBLICPATH);
+  }
 
   public route(req: http.IncomingMessage, res: http.ServerResponse) {
     const requestedURL = req.url;
+    this.logRequest(req);
+
     if (requestedURL) {
-      if (Object.keys(config.ROUTES).includes(requestedURL)) {
-        this.fileResponse(res, 200, config.ROUTES[requestedURL]);
+      if (Object.keys(this.routeMap).includes(requestedURL)) {
+        this.fileResponse(res, 200, this.routeMap[requestedURL]);
+      } else if (requestedURL == "/") {
+        this.fileResponse(res, 200, this.routeMap["/index.html"]);
       } else {
-        this.fileResponse(res, 404, config.ROUTES["/404.html"]);
+        this.fileResponse(res, 404, this.routeMap["/error/404.html"]);
       }
     } else {
-      this.fileResponse(res, 404, config.ROUTES["/404.html"]);
+      this.fileResponse(res, 404, this.routeMap["/error/404.html"]);
     }
+  }
+
+  private logRequest(req: http.IncomingMessage) {
+    console.log(req.socket.remoteAddress, req.method, req.url);
+  }
+
+  private logResponse(res: http.ServerResponse, filePath: string) {
+    console.log(res.statusCode, filePath);
   }
 
   private fileResponse(
@@ -27,10 +44,32 @@ export class Router {
     res.setHeader("Content-Type", "text/html");
     fs.readFile(filePath, (error, data) => {
       if (error) {
-        console.log(error);
+        console.error(error);
       }
       res.write(data);
       res.end();
+      this.logResponse(res, filePath);
     });
+  }
+
+  private createRouteMap(directory: string) {
+    fs.readdir(directory, (error, files) => {
+      if (error) {
+        console.error(error);
+      } else {
+        files.forEach((file) => {
+          const fullPath = path.join(directory, file);
+          const isDirectory = fs.lstatSync(fullPath).isDirectory();
+
+          if (isDirectory) {
+            this.createRouteMap(fullPath);
+          } else {
+            const relativePath = fullPath.split(config.PUBLICPATH)[1];
+            this.routeMap[relativePath] = fullPath;
+          }
+        });
+      }
+    });
+    return this.routeMap;
   }
 }
